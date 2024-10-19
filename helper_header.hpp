179fb271a9429344bfcc9f119700be175b8c9fdc
martyn <martyn@iconbuild.com>
2024-10-19 14:42:57 -0500

    lib

---
+++ helper_header.hpp
@@ -0,0 +1,11 @@
+#ifndef helper_header_hpp
+#define helper_header_hpp
+
+const char msg_start_sentinel = '?';
+const char msg_end_sentinel = '!';
+
+const char msg_chunk_delimiter = '_';
+
+
+
+#endif  // helper_header_hpp
